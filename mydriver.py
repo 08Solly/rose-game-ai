7949fa6e13f1c4311a341eafa237f8bf76d1ca8e
08Solly <08solly@gmail.com>
2025-07-21 16:27:55 +0300

    findPenguin-obstacles

--- mydriver.py
+++ mydriver.py
@@ -16,10 +16,18 @@ def escape_obstacles(world) -> str:
     x = world.car.x
     y = world.car.y
 
+    found_penguin = findPenguin(world, x, y)
+    found_water = findWater(world, x, y)
+    found_crack = findCracks(world, x, y)
 
-    found_penguin = findPenguin(world,x,y)
     if found_penguin != actions.NONE:
         return found_penguin
+
+    if found_crack != actions.NONE:
+        return found_crack
+
+    if found_water != actions.NONE:
+        return found_water
 
     if is_obstacle(world.get((x, y - 1))):
         # left lane
@@ -61,38 +69,88 @@ def brake(obs) -> str:
         return actions.PICKUP
     else:
         return actions.NONE
-    
 
-def findPenguin(world: list[int, int],pos_x,pos_y: int):
+
+def findCracks(world: list[int, int], pos_x, pos_y: int):
     """
     Finds penguins in the world.\n
     Return the position of the penguin if found, otherwise return (-1,-1)
     """
 
-    
     try:
-        if world.get((pos_x, pos_y - 1)) == obstacles.PENGUIN:
-            return actions.PICKUP
-        
-        if world.get((pos_x-1, pos_y-2)) == obstacles.PENGUIN and is_obstacle(world.get((pos_x-1, pos_y-1)))==False:
-            return actions.LEFT
-        if world.get((pos_x+1, pos_y-2)) == obstacles.PENGUIN and is_obstacle(world.get((pos_x+1, pos_y-1)))==False:
-            return actions.RIGHT
-        
-        if world.get((pos_x-2, pos_y-3)) == obstacles.PENGUIN and is_obstacle(world.get((pos_x-2, pos_y-2)))==False:
-            return actions.LEFT
-        
-        if world.get((pos_x+2, pos_y-3)) == obstacles.PENGUIN and is_obstacle(world.get((pos_x+2, pos_y-2)))==False:
-            return actions.RIGHT
+        if world.get((pos_x, pos_y - 1)) == obstacles.CRACK:
+            return actions.JUMP
 
-        if world.get((pos_x-2, pos_y-3)) == obstacles.PENGUIN and is_obstacle(world.get((pos_x-2, pos_y-2)))==False:
-            return actions.LEFT
+        if world.get((pos_x + 1, pos_y - 2)) == obstacles.CRACK:
+            if not is_obstacle(world.get((pos_x + 1, pos_y - 1))):
+                if pos_x + 1 < 3:
+                    return actions.RIGHT
 
+        if world.get((pos_x - 1, pos_y - 2)) == obstacles.CRACK:
+            if not is_obstacle(world.get((pos_x - 1, pos_y - 1))):
+                if pos_x - 1 >= 0:
+                    return actions.LEFT
 
-    except IndexError:    
+    except IndexError:
         return actions.NONE
 
     return actions.NONE
+
+
+def findWater(world: list[int, int], pos_x, pos_y: int):
+    """
+    Finds penguins in the world.\n
+    Return the position of the penguin if found, otherwise return (-1,-1)
+    """
+
+    try:
+        if world.get((pos_x, pos_y - 1)) == obstacles.WATER:
+            return actions.BRAKE
+
+        if world.get((pos_x + 1, pos_y - 2)) == obstacles.WATER:
+            if not is_obstacle(world.get((pos_x + 1, pos_y - 1))):
+                if pos_x + 1 < 3:
+                    return actions.RIGHT
+
+        if world.get((pos_x - 1, pos_y - 2)) == obstacles.WATER:
+            if not is_obstacle(world.get((pos_x - 1, pos_y - 1))):
+                if pos_x - 1 >= 0:
+                    return actions.LEFT
+
+    except IndexError:
+        return actions.NONE
+
+    return actions.NONE
+
+
+def findPenguin(world: list[int, int], pos_x, pos_y: int):
+    """
+    Finds water in the world.\n
+    Return the position of the water if found, otherwise return (-1,-1)
+    """
+
+    try:
+        if world.get((pos_x, pos_y - 1)) == obstacles.PENGUIN:
+            return actions.PICKUP
+
+        if world.get((pos_x + 1, pos_y - 2)) == obstacles.PENGUIN:
+            if not is_obstacle(world.get((pos_x + 1, pos_y - 1))) and (world.get((pos_x + 1, pos_y - 1)) not in (obstacles.WATER,obstacles.CRACK)):
+                if pos_x + 1 < 3:
+                    return actions.RIGHT
+
+        if world.get((pos_x - 1, pos_y - 2)) == obstacles.PENGUIN:
+            if not is_obstacle(world.get((pos_x - 1, pos_y - 1))) and (world.get((pos_x - 1, pos_y - 1)) not in (obstacles.WATER,obstacles.CRACK)):
+                if pos_x - 1 >= 0:
+                    return actions.LEFT
+
+
+
+    except IndexError:
+        return actions.NONE
+
+    return actions.NONE
+
+
 
 
 
