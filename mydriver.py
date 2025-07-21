1e1d2a6c47387280f9afd915a30e47cc60e8ab73
08Solly <08solly@gmail.com>
2025-07-21 12:11:06 +0300

    driving (WIP)

--- mydriver.py
+++ mydriver.py
@@ -4,33 +4,89 @@ driver_name = "MyDriver"
 
 
 def drive(world):
+    map = map_world(world)
+
+    return escape(map,world)
+
+
+# def findPenguin(mapped_world: list[int, int], pos_y: int) -> tuple[int, int]:
+#     """
+#     Finds penguins in the world.\n
+#     Return the position of the penguin if found, otherwise return (-1,-1)
+#     """
+#
+#     for x_pos in range(len(mapped_world[0])):
+#         if mapped_world[pos_y - 1][x_pos] == obstacles.PENGUIN:
+#             return (x_pos, pos_y - 1)
+#
+#     for x_pos in range(len(mapped_world[0])):
+#         if not obstacle_T_F(mapped_world[pos_y - 1][[x_pos]]):
+#             if mapped_world[pos_y][x_pos] == obstacles.PENGUIN:
+#                 return (x_pos, pos_y)
+#
+#     return (-1, -1)
+
+
+def map_world(world) -> list[list[str]]:
+    car_x: int = world.car.x
+    car_y: int = world.car.y
+
+    lanes: tuple[int] = (0, 1, 2)
+
+    # forward offset of the look by car
+    forward_look: int = 3
+
+    # get for every lane the obstacle in it for 3x3 car in bottom
+    lane_matrix: list[list[str]] = [
+        [(world.get((x, y)), y) for x in lanes] for y in range((car_y - forward_look -1), car_y - 1)]
+
+    poses = [([x for x in lanes], y) for y in range((car_y - forward_look -1 ), car_y - 1)]
+    [print(row) for row in poses]
+    [print(row) for row in lane_matrix]
+    print(f"x:{car_x}, y:{car_y}")
+
+    return lane_matrix
+
+def escape(map,world):
     x = world.car.x
     y = world.car.y
 
-    try:
-        obstacle = world.get((x, y - 1))
-    except IndexError:
-        return actions.BRAKE
-
-    if obstacle == obstacles.NONE:
-        return actions.NONE
-    elif obstacle == obstacles.PENGUIN:
-        return actions.PICKUP
-    elif obstacle == obstacles.WATER:
-        return actions.BRAKE
-    elif obstacle == obstacles.CRACK:
-        return actions.JUMP
-    elif obstacle in (obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER):
-        right_obstacle = world.get((x + 1, y))
-        left_obstacle = world.get((x - 1, y))
-        if right_obstacle == obstacles.NONE and x+1 <= 2:
+    # penguin_pos = findPenguin(map,y)
+    # if penguin_pos != (-1,-1):
+    #     if penguin_pos[0] > x:
+    #         return actions.RIGHT
+    #     if penguin_pos[0] < x:
+    #         return actions.LEFT
+    if x==0:
+        if (obstacle_T_F(map[2][x])==True and obstacle_T_F(map[2][x+1])==True) and (obstacle_T_F(map[1][x]) == False and obstacle_T_F(map[1][x+1]) == False):
             return actions.RIGHT
-        elif left_obstacle == obstacles.NONE:
+        elif obstacle_T_F(map[1][x]) == True:
+                return actions.RIGHT
+        return actions.NONE
+
+    elif x==2:
+        if (obstacle_T_F(map[2][x])==True and obstacle_T_F(map[2][x-1])==True) and (obstacle_T_F(map[1][x]) == False and obstacle_T_F(map[1][x-1]) == False):
             return actions.LEFT
-        else:
-            return actions.NONE
+        elif obstacle_T_F(map[1][x]) == True:
+                return actions.LEFT
+        return actions.NONE
 
-    return actions.BRAKE
+    elif x==1:
+        if (obstacle_T_F(map[2][x])==True and obstacle_T_F(map[2][x-1])==True) and (obstacle_T_F(map[1][x]) == False and obstacle_T_F(map[1][x+1]) == False):
+            return actions.RIGHT
+        elif (obstacle_T_F(map[2][x])==True and obstacle_T_F(map[2][x+1])==True) and (obstacle_T_F(map[1][x]) == False and obstacle_T_F(map[1][x-1]) == False):
+                return actions.LEFT
+        elif obstacle_T_F(map[1][x]) == True:
+            if obstacle_T_F(map[1][x+1]) == False:
+                return actions.RIGHT
+            else:
+                return actions.LEFT
+        return actions.NONE
 
 
+def obstacle_T_F(obs):
+    if obs != obstacles.NONE or obs != obstacles.PENGUIN:
+        return True
+
+    return False
 
