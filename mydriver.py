c30ec7d5022b0d13cb83e9ab455aa3fecdef13f5
tal680 <talmaromgross@gmail.com>
2025-07-21 11:42:44 +0300

    mapping

--- mydriver.py
+++ mydriver.py
@@ -1,9 +1,33 @@
 from rose.common import obstacles, actions  # NOQA
 
 driver_name = "MyDriver"
+lane = -1
+
+def map_world(world) -> list[list[str]]:
+    car_x: int = world.car.x
+    car_y: int = world.car.y
+
+    lanes: tuple[int] = (0, 1, 2)
+
+    # forward offset of the look by car
+    forward_look: int = 3
+
+    # get for every lane the obstacle in it for 3x3 car in bottom
+    lane_matrix: list[list[str]] = [
+        [(world.get((x, y)), y) for x in lanes] for y in range((car_y - forward_look -1), car_y - 1)]
+
+    # testing
+    # poses = [([x for x in lanes], y) for y in range((car_y - forward_look -1 ), car_y - 1)]
+    # [print(row) for row in poses]
+    # [print(row) for row in lane_matrix]
+    # print(f"x:{car_x}, y:{car_y}")
+
+    return lane_matrix
+
 
 
 def drive(world):
+
     x = world.car.x
     y = world.car.y
 
